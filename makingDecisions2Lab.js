e3251c29ff5895d7d88c350d655e21a721c3622b
Cory Owen <cjowen14@outlook.com>
2021-12-16 13:23:27 -0700

    Finished Problem 5

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -65,6 +65,17 @@ var score = 74
 */
 
 //Code Here
+if (score >= 90){
+  console.log(`A`);
+} else if (score <= 89 && score >= 80){
+  console.log(`B`);
+} else if (score <= 79 && score >= 70){
+  console.log (`C`);
+} else if (score <=69 && score >=60){
+  console.log(`D`);
+} else{
+  console.log(`F`);
+}
 
 
 ////////// Intermediate Problems //////////
